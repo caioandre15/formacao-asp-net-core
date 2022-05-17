e22e4d96efc23b052c78114b58484d0ca660158e
Caio André <caio_andre15@hotmail.com>
2022-05-16 23:12:15 -0300

    Creating a Web API and implementing a CRUD.

---
+++ curso-02-rest-com-asp-net-core-web-api/MinhaAPI-Net-Core-2-2/MinhaAPI-Net-Core-2-2/Models/ApiDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MinhaAPI_Net_Core_2_2.Models
+{
+    public class ApiDbContext : DbContext
+    {
+        public ApiDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+
+    }
+}
