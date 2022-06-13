edbdc2f9664e1c9f598449d3026b28b4749be449
Caio André <caio_andre15@hotmail.com>
2022-06-13 20:33:29 -0300

    Implementing the JWT

---
+++ curso-02-rest-com-asp-net-core-web-api/MinhaAPICompleta/src/DevIO.Api/Configuration/AppSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevIO.Api.Configuration
+{
+    public class AppSettings
+    {
+
+        public string Secret { get; set; } //Nossa chave de criptografia
+        public int ExpiracaoHoras { get; set; }// Quantas horas o token vai levar até perder a validade
+        public string Emissor { get; set; }// Quem emite, no a aplicação
+        public string ValidoEm { get; set; }// Qm quais URL este token é valido
+
+    }
+}
--- curso-02-rest-com-asp-net-core-web-api/MinhaAPICompleta/src/DevIO.Api/Controllers/FornecedoresController.cs
+++ curso-02-rest-com-asp-net-core-web-api/MinhaAPICompleta/src/DevIO.Api/Controllers/FornecedoresController.cs
@@ -2,6 +2,7 @@
 using DevIO.Api.ViewModels;
 using DevIO.Business.Intefaces;
 using DevIO.Business.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -10,7 +11,7 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace DevIO.Api.Controllers
-{
+{    
     [Route("api/[controller]")]
     public class FornecedoresController : MainController
     {
