73d64a2a71f949d5d24bea675c00e226869e5211
Caio André <caio_andre15@hotmail.com>
2022-08-05 08:17:40 -0300

    Implementing routing by parameter

--- curso-03-dominando-o-aspnet-mvc-core/MinhaDemoMvc/MinhaDemoMvc/Controllers/HomeController.cs
+++ curso-03-dominando-o-aspnet-mvc-core/MinhaDemoMvc/MinhaDemoMvc/Controllers/HomeController.cs
@@ -8,19 +8,28 @@ using System.Threading.Tasks;
 
 namespace MinhaDemoMvc.Controllers
 {
+    [Route("")]
+    [Route("gestao-clientes")]
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        [Route("")]
+        [Route("pagina-inicial")]
+        [Route("pagina-inicial/{id:int}/{categoria?}")]
+        public IActionResult Index(int id, string categoria)
         {
             return View();
         }
 
+        [Route("politica-de-privacidade")]
+        [Route("privacidade")] // Na sobre carga das rotas a última sempre vai prevalecer as anteriores.
         public IActionResult Privacy()
         {
             return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        
+        [Route("erro-encontrado")]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
